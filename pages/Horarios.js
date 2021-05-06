0e0b56b4aa2223ee31de749052f74efeac324099
Oswaldo <oquinatoav@gmail.com>
2021-05-05 21:28:45 -0500

    v1.01

--- pages/Horarios.js
+++ pages/Horarios.js
@@ -4,6 +4,15 @@ import List from '@material-ui/core/List';
 import React from 'react'
 import Header from '../components/Header/Header'
 
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
 
 import CeldaLista from '../components/CeldaLista/CeldaLista'
 
